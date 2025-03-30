9ab3a2e06fc3cc56643413001ec692db74458cd6
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-29 21:18:48 -0500

    Implemented addLiquidity function

--- foundry/foundry-app/src/Exchange.sol
+++ foundry/foundry-app/src/Exchange.sol
@@ -18,4 +18,52 @@ contract Exchange is ERC20 {
         return ERC20(tokenAddress).balanceOf(address(this));
     }
 
+    // addLiquidity allows users to add liquidity to the exchange
+    function addLiquidity(
+        uint256 amountOfToken
+    ) public payable returns (uint256) {
+        uint256 lpTokensToMint;
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 tokenReserveBalance = getReserve();
+
+        ERC20 token = ERC20(tokenAddress);
+
+        // If the reserve is empty, take any user supplied value for initial liquidity
+        if (tokenReserveBalance == 0) {
+            // Transfer the token from the user to the exchange
+            token.transferFrom(msg.sender, address(this), amountOfToken);
+
+            // lpTokensToMint = ethReserveBalance = msg.value
+            lpTokensToMint = ethReserveBalance;
+
+            // Mint LP tokens to the user
+            _mint(msg.sender, lpTokensToMint);
+
+            return lpTokensToMint;
+        }
+
+        // If the reserve is not empty, calculate the amount of LP Tokens to be minted
+        uint256 ethReservePriorToFunctionCall = ethReserveBalance - msg.value;
+        uint256 minTokenAmountRequired = (msg.value * tokenReserveBalance) /
+            ethReservePriorToFunctionCall;
+
+        require(
+            amountOfToken >= minTokenAmountRequired,
+            "Insufficient amount of tokens provided"
+        );
+
+        // Transfer the token from the user to the exchange
+        token.transferFrom(msg.sender, address(this), minTokenAmountRequired);
+
+        // Calculate the amount of LP tokens to be minted
+        lpTokensToMint =
+            (totalSupply() * msg.value) /
+            ethReservePriorToFunctionCall;
+
+        // Mint LP tokens to the user
+        _mint(msg.sender, lpTokensToMint);
+
+        return lpTokensToMint;
+    }
+
 }
